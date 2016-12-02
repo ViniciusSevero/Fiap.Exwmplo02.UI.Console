9f7e724e88b97807db36aa0beffa9647a2c27d4c
Aluno Shift <alunoshift@fiap.com.br>
2016-12-02 17:59:42 -0200

    First commit

---
+++ Fiap.Exwmplo02.UI.Console/Program.cs
@@ -0,0 +1,108 @@
+﻿using Fiap.Exwmplo02.UI.Console.DTOs;
+using Fiap.Exwmplo02.UI.Console.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Exwmplo02.UI.Console
+{
+    class Program
+    {
+        private static Uri _uri = new Uri("http://localhost:64015/");
+        private static AlunoRepository _rep = new AlunoRepository();
+
+        public static void Main(string[] args)
+        {
+            int resp = 0;
+
+            while(resp != 2)
+            {
+                System.Console.WriteLine("Digite corretamente \n 0 - Cadastrar \n 1 - Listar \n 2 - Sair");
+                resp = Convert.ToInt32(System.Console.ReadLine());
+                while (resp != 0 && resp != 1 && resp != 2)
+                {
+                    System.Console.WriteLine("Digite corretamente\n  0 - Cadastrar \n 1 - Listar \n 2 - Sair");
+                    resp = Convert.ToInt32(System.Console.ReadLine());
+                }
+
+                if(resp == 0)
+                {
+                    Cadastrar();
+                }
+                else
+                {
+                    if(resp == 1)
+                    {
+                        Listar();
+                    }
+                }
+            }
+        }
+
+        public static void Cadastrar()
+        {
+           
+            System.Console.WriteLine("Nome");
+            string nome = System.Console.ReadLine();
+
+            System.Console.WriteLine("Bolsa S/M");
+            bool bolsa = false;
+            double desconto = 0;
+            if (System.Console.ReadLine().ToUpper().Equals("S"))
+            {
+                bolsa = true;
+
+                System.Console.WriteLine("Desconto");
+                desconto = Convert.ToDouble(System.Console.ReadLine());
+            }
+            else
+            {
+                bolsa = false;
+            }
+
+            System.Console.WriteLine("Cep");
+            string cep = System.Console.ReadLine();
+
+            System.Console.WriteLine("Grupo ID");
+            int grupoId = Convert.ToInt32(System.Console.ReadLine());
+
+            System.Console.WriteLine("Data de Nascimento");
+            string[] explode = System.Console.ReadLine().Split('/');
+            DateTime data = new DateTime(Convert.ToInt32(explode[2]), Convert.ToInt32(explode[1]), Convert.ToInt32(explode[0]));
+
+            AlunoDTO aluno = new AlunoDTO()
+            {
+                Bolsa = bolsa,
+                Cep = cep,
+                DataNascimento = data,
+                Desconto = desconto,
+                Nome = nome,
+                GrupoId = grupoId
+            };
+
+
+            _rep.Cadastrar(aluno, _uri);
+
+        }
+
+        public static void Listar()
+        {
+
+            IEnumerable<AlunoDTO> lista = _rep.Listar(_uri);
+
+            foreach (var aluno in lista)
+            {
+                System.Console.WriteLine("Id = "+aluno.Id);
+                System.Console.WriteLine("Nome = " + aluno.Nome);
+                System.Console.WriteLine("DataNascimento = " + aluno.DataNascimento);
+                System.Console.WriteLine("GrupoId = " + aluno.GrupoId);
+                System.Console.WriteLine("Bolsa = " + aluno.Bolsa);
+                System.Console.WriteLine("Desconto = " + aluno.Desconto);
+                System.Console.WriteLine("Cep = " + aluno.Cep);
+                System.Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ Fiap.Exwmplo02.UI.Console/Repositories/AlunoRepository.cs
@@ -0,0 +1,55 @@
+﻿using Fiap.Exwmplo02.UI.Console.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Exwmplo02.UI.Console.Repositories
+{
+    class AlunoRepository
+    {
+        public Uri Cadastrar(AlunoDTO aluno,Uri address)
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = address;
+
+                HttpResponseMessage response = client.PostAsJsonAsync("api/Aluno", aluno).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Uri uri = response.Headers.Location;
+                    return uri;
+                }
+
+                return null;
+            }
+        }
+
+         public IEnumerable<AlunoDTO> Listar(Uri address)
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = address;
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = client.GetAsync("api/Aluno").Result;
+
+                IEnumerable<AlunoDTO> alunos = null;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    alunos = response.Content.ReadAsAsync<IEnumerable<AlunoDTO>>().Result;
+                }
+
+                return alunos;
+            }
+        }
+    }
+}
